5a25a208a188ad8812c376eff6992f609f3c52c0
Thomas Haller <thomashaller@gmx.at>
2022-10-18 13:58:20 +0200

    implemented nft in a basic version with 6 tests.

--- contracts/DemoNFT.sol
+++ contracts/DemoNFT.sol
@@ -8,9 +8,9 @@ contract DemoNFT is ERC721 {
     uint256 private _currentTokenId = 0; //Token ID here will start from 1
 
     // salt value that is used uniquely for salting the rng for each minted nft.
-    uint256 private _current_minting_registry_salt = 1;
+    uint256 private _currentMintingRegistrySalt = 1;
 
-    uint256 public minting_fee = 1 ether;
+    uint256 public mintingFee = 1 ether;
 
     // a minting registry holds the information about upcomming minting of nfts.
     // one account can only mint one NFT at a time.
@@ -21,77 +21,82 @@ contract DemoNFT is ERC721 {
     // the salt is connected to _minting_registry_blocks and prevents manipulation of the rng
     mapping(address => uint256) public _minting_registry_salts;
 
-    bytes32[] public _token_dna;
-    // modifier onlyEOA() {
-    //     require(msg.sender == tx.origin, "TestNFT: must use EOA");
-    //     _;
-    // }
+    mapping(uint256 => bytes32) public tokenDna;
 
     IRandomHbbft private _random_hbbft;
+
+    // mint registered event, 
+    // emitted when a minting is registered
+    // block_number is the block number of the block where minting becomes possible.
+    event MintRegistered(address indexed account, uint256 blockNumber);
 
     constructor(address random_hbbft_address) ERC721("DemoNFT", "DEMO") {
         _random_hbbft = IRandomHbbft(random_hbbft_address);
     }
 
-    function register_minting() public payable {
+    /** @dev registers a minting of an NFT, paying the minting fee.
+     * the receiver of the NFT can be an account other than the account who pays the fee.
+     * Only one mint can be registered at a time.
+     * @param _to the account that will receive the NFT.
+     */
+    function registerMinting(address _to) public payable {
         // if there is already a minting registered for the sender, then the minting is not allowed.
         require(
-            _minting_registry_blocks[msg.sender] == 0,
-            "TestNFT: minting already registered"
+            _minting_registry_blocks[_to] == 0,
+            "DemoNFT: minting already registered"
         );
 
         // the minting fee has to be paid for registering the minting.
-        require(msg.value >= minting_fee, "TestNFT: not enough ether sent");
+        require(msg.value == mintingFee, "DemoNFT: must send exact minting fee");
 
         // the minting can only happen in the future,
         // all minting fees have to be paid during the registration.
-        _minting_registry_blocks[msg.sender] = block.number + 1;
-        _minting_registry_salts[msg.sender] = _current_minting_registry_salt;
-        _current_minting_registry_salt++;
+        _minting_registry_blocks[_to] = block.number + 1;
+        _minting_registry_salts[_to] = _currentMintingRegistrySalt;
+        _currentMintingRegistrySalt++;
+
+        emit MintRegistered(_to, block.number + 1);
     }
 
     /**
      * @dev Mints a token to an address with a tokenURI.
      * The minting will fail if it has not been registered yet.
+     * Minting can be called by anyone who has registered the minting.
      * @param _to address of the future owner of the token
      */
     function mintTo(address _to) public {
 
-        uint256 block_number = _minting_registry_blocks[msg.sender];
+        uint256 blockNumber = _minting_registry_blocks[_to];
 
         // if there is no minting registered for the sender, then the minting is not allowed.
         require(
-            block_number != 0,
-            "TestNFT: minting not registered"
+            blockNumber != 0,
+            "DemoNFT: minting not registered"
         );
-
 
         require(
-            block.number >= block_number,
+            block.number >= blockNumber,
             "RNG number for ready yet."
         );
-
         
         // clear the minting registry for this sender.
-        _minting_registry_blocks[msg.sender] = 0;
-        _minting_registry_salts[msg.sender] = 0;
+        _minting_registry_blocks[_to] = 0;
+        _minting_registry_salts[_to] = 0;
 
         uint256 newTokenId = _getNextTokenId();
         _safeMint(_to, newTokenId);
 
-        
-
         // the salt makes sure that registered mints for the same block do not result in the same DNA.
         // every salt is only used once.
-        uint256 salt = _minting_registry_salts[msg.sender];
+        uint256 salt = _minting_registry_salts[_to];
 
         // get the RNG that has been written in the past. (including the same block, but the RNG transaction is the same)
-        uint256 rng = _random_hbbft.get_seed_historic(block_number);
+        uint256 rng = _random_hbbft.get_seed_historic(blockNumber);
 
-        _token_dna[newTokenId] = keccak256(abi.encodePacked(rng + salt));
+        tokenDna[newTokenId] = (keccak256(abi.encodePacked(rng, salt)));
+        // according to the rule of big numbers, rng + salt should realistically never overflow and be unique.
 
         _incrementTokenId();
-
     }
 
     /**
--- test/index.ts
+++ test/index.ts
@@ -1,31 +1,81 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { TestNFT, TestNFT__factory } from "../typechain";
+import {
+  DemoNFT,
+  DemoNFT__factory,
+  DummyRandomHbbft,
+  DummyRandomHbbft__factory,
+} from "../typechain";
 
 describe("NFT", function () {
   let signers: SignerWithAddress[] | undefined;
   let main: string = "";
-  let NFT: TestNFT__factory | undefined;
+  let NFT: DemoNFT__factory | undefined;
+  let RNG: DummyRandomHbbft__factory | undefined;
 
   before(async () => {
     signers = await ethers.getSigners();
     main = signers[0].address;
-    NFT = await ethers.getContractFactory("TestNFT");
+    NFT = await ethers.getContractFactory("DemoNFT");
+    RNG = await ethers.getContractFactory("DummyRandomHbbft");
   });
 
-  let nft: TestNFT | undefined;
+  let rng: DummyRandomHbbft | undefined;
+  let nft: DemoNFT | undefined;
 
   it("deploy contract", async function () {
-    nft = await NFT?.deploy();
+    rng = await RNG?.deploy();
+    nft = await NFT?.deploy(rng?.address!);
     await nft?.deployed();
   });
 
-  it("mint increases nft count", async () => {
+  it("minting should fail if not registered", async () => {
     if (nft) {
-      const mintTX = await nft.mintTo(main);
+      await expect(nft.mintTo(main)).revertedWith(
+        "DemoNFT: minting not registered"
+      );
     }
 
+    // console.log("mintTX:", mintTX?.hash);
+  });
+
+  it("registering minting should fail if not enough minting fee is provided.", async () => {
+    if (nft) {
+      await expect(nft.registerMinting(main)).revertedWith(
+        "DemoNFT: must send exact minting fee"
+      );
+    }
+    // console.log("mintTX:", mintTX?.hash);
+  });
+
+  it("registering minting should succeed if  enough minting fee is provided.", async () => {
+    if (nft) {
+      await nft.registerMinting(main, { value: ethers.utils.parseEther("1") });
+    }
+    // console.log("mintTX:", mintTX?.hash);
+  });
+
+  it("registering minting should fail if already registered.", async () => {
+    if (nft) {
+      await expect(
+        nft.registerMinting(main, { value: ethers.utils.parseEther("1") })
+      ).revertedWith("DemoNFT: minting already registered");
+    }
+    // console.log("mintTX:", mintTX?.hash);
+  });
+
+  it("minting should succeed if registered.", async () => {
+    if (nft) {
+      await nft.mintTo(main);
+      const tokenID = 1;
+      const owner = await nft.ownerOf(tokenID);
+      expect(owner).to.equal(main);
+      const tokenDna = await nft.tokenDna(tokenID);
+      expect(tokenDna).not.to.equal(
+        "0x0000000000000000000000000000000000000000000000000000000000000000"
+      );
+    }
     // console.log("mintTX:", mintTX?.hash);
   });
 });
